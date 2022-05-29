bba78eb026a7e238a8e84124475e9e6794a2f68a
Ade Muslim <69449808+amuslim12@users.noreply.github.com>
2022-05-29 10:13:02 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
-$user = "root";
-$pass = "";
+$user = "adminklinik";
+$pass = "312010147";
 $db = "klinik_312010147";
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 if ($koneksi == false)
