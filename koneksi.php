9f3b8dae3a4c1f857f074c941269e7d14622a1a5
Ade Muslim <69449808+amuslim12@users.noreply.github.com>
2022-06-19 14:22:23 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -6,20 +6,11 @@ $db = "klinik_312010147";
 
 //Buat variable koneksi untuk menghubungkan database dgn PHP
 $koneksi = mysqli_connect($host, $user, $pass, $db); //isi dgn 4 variable
-//Cek koneksi
-if ($koneksi){
-    $buka=mysqli_select_db($db);
-    echo "Database dapat terhubung";
-    if (!$buka){
-        echo "Database tidak dapat terhubung";
-    }
-}else{
-    echo "MySQL tidak terhubung";
-}
 
-// if ($koneksi == false)
-// {
-//     echo "Koneksi ke server gagal.";
-//     die();
-// }
+//Cek koneksi
+if ($koneksi == false)
+{
+    echo "Koneksi ke server gagal.";
+    die();
+}
 ?>
